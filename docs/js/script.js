5b954cd741fce55c6fe97ab7b5686cbd3489d7c7
Gallent-Jiraiya <tharindurcg1@gmail.com>
2021-07-13 16:17:07 +0530

    respons

--- docs/js/script.js
+++ docs/js/script.js
@@ -520,10 +520,13 @@ document.addEventListener("DOMContentLoaded", function() { // On DOM Load initia
 /** End of Things to remember***/
 let imgs=document.getElementsByClassName('award-img');
 for (var i = 0; i < imgs.length; i++) {
-    imgs[i].style.height = imgs[i].style.width;
+let imgs=document.getElementsByClassName('award-img');
+    imgs[i].style.height = imgs[i].clientWidth;
+    console.log(imgs[i].clientWidth);
 }
 let uokimgs=document.getElementsByClassName('uok-img');
 for (var i = 0; i < uokimgs.length; i++) {
-    uokimgs[i].style.height = uokimgs[i].style.width;
+    uokimgs[i].style.height = uokimgs[i].clientWidth;
+    console.log(uokimgs[i].clientWidth);
 }
     
