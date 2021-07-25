fc4639165e90c1e0d6733bb014c3d1348a44e60c
Gallent-Jiraiya <tharindurcg1@gmail.com>
2021-07-26 00:41:14 +0530

    registration

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -86,7 +86,9 @@ function ShowImage(path){
   .addTo(controller);
 }
 
+
 !(function($) {
+   
   "use strict";
   // Back to top button
   $(window).scroll(function() {
@@ -244,6 +246,30 @@ function ShowImage(path){
     var ticketType = button.data('ticket-type');
     var modal = $(this);
     modal.find('#ticket-type').val(ticketType);
-  })
+  });
+  // Get the modal
+var modal = document.getElementById("myModal");
+        
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("modalClose")[0];
+
+// When the user clicks the button, open the modal 
+$(btn).click (function() {
+  modal.style.display = "block";
+});
+
+// When the user clicks on <span> (x), close the modal
+$(span).click (function() {
+  modal.style.display = "none";
+});
+
+// When the user clicks anywhere outside of the modal, close it
+$(window).click(function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
 })(jQuery);
