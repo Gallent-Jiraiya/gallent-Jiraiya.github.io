2f65269c6d63400dbeac1547d64abf3fd5ceb1db
Gallent-Jiraiya <tharindurcg1@gmail.com>
2021-08-22 13:22:44 +0530

    Car-Rent veh

--- docs/dbConnection.php
+++ docs/dbConnection.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "assesment","3306");
+$conn = mysqli_connect("localhost", "root", "", "carrent","3306");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
---
+++ docs/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="js/main.js"></script>
+    </head>
+    <body>
+
+    <div id="head_bar">
+        <img src="img/Carsons Logo white.png">
+    </div>
+    <div id="banner">
+
+    </div>
+    <div id="search">
+        <span>
+        <label>Start Date</label>
+        <input type="date" name="startDate">
+        </span>
+        <span>
+        <label>End Date</label>
+        <input type="date" name="startDate">
+        </span>
+        <input type="submit" value="Check Available Vehicles">
+    </div>
+    <div id="about">
+        <div id="abt_topic">
+            <p>Enjoy the efficient and specialized services of Casons Rent a Car; Sri Lanka's leading rent-a-car company
+            </p>
+        </div>
+        <div id="cards">
+            <div class="card">
+                <i class="fas fa-car-alt"></i>
+                <p class="mini"> Over 500 Vehicles</p>
+                <p>Over 60 4WD Jeeps, 50 Vans, 200 Cars, Double Cabs, Luxury Coaches, Lorries etc</p>
+            </div>
+           <div class="card">
+           <i class="fas fa-user"></i>
+            <p class="mini"> Our Strengh</p>
+            <p>30 In-house Operational Staff, 18 Automobile Technicians and 90 permanent Drivers from every part of the Island</p>
+            </div>
+           <div class="card">
+           <i class="fas fa-star"></i>
+            <p class="mini"> Insurance</p>
+            <p>Comprehensive Insurance available for all our vehicles including Passengers</p>
+            </div>
+           <div class="card">
+           <i class="fas fa-user"></i>
+            <p class="mini"> 24/7 Breakdown Service</p>
+            <p>24 hrs Island wide backup services with affiliated Garages and Mobile units</p>
+            </div>
+           
+            
+        </div>
+    </div>
+    <div class="topic">
+            Our Vehicles
+        </div>
+    <div id="vehicles">
+        
+        <div id="cars">
+        <div class="card">
+                <i class="fas fa-car-alt"></i>
+                <p class="mini"> Over 500 Vehicles</p>
+                <p>Over 60 4WD Jeeps, 50 Vans, 200 Cars, Double Cabs, Luxury Coaches, Lorries etc</p>
+            </div>
+            <div class="card">
+                <i class="fas fa-car-alt"></i>
+                <p class="mini"> Over 500 Vehicles</p>
+                <p>Over 60 4WD Jeeps, 50 Vans, 200 Cars, Double Cabs, Luxury Coaches, Lorries etc</p>
+            </div>
+            <div class="card">
+                <i class="fas fa-car-alt"></i>
+                <p class="mini"> Over 500 Vehicles</p>
+                <p>Over 60 4WD Jeeps, 50 Vans, 200 Cars, Double Cabs, Luxury Coaches, Lorries etc</p>
+            </div>
+            <div class="card">
+                <i class="fas fa-car-alt"></i>
+                <p class="mini"> Over 500 Vehicles</p>
+                <p>Over 60 4WD Jeeps, 50 Vans, 200 Cars, Double Cabs, Luxury Coaches, Lorries etc</p>
+            </div>
+            <div class="card">
+                <i class="fas fa-car-alt"></i>
+                <p class="mini"> Over 500 Vehicles</p>
+                <p>Over 60 4WD Jeeps, 50 Vans, 200 Cars, Double Cabs, Luxury Coaches, Lorries etc</p>
+            </div>
+            <div class="card">
+                <i class="fas fa-car-alt"></i>
+                <p class="mini"> Over 500 Vehicles</p>
+                <p>Over 60 4WD Jeeps, 50 Vans, 200 Cars, Double Cabs, Luxury Coaches, Lorries etc</p>
+            </div>
+            <div class="card">
+                <i class="fas fa-car-alt"></i>
+                <p class="mini"> Over 500 Vehicles</p>
+                <p>Over 60 4WD Jeeps, 50 Vans, 200 Cars, Double Cabs, Luxury Coaches, Lorries etc</p>
+            </div>
+            <div class="card">
+                <i class="fas fa-car-alt"></i>
+                <p class="mini"> Over 500 Vehicles</p>
+                <p>Over 60 4WD Jeeps, 50 Vans, 200 Cars, Double Cabs, Luxury Coaches, Lorries etc</p>
+            </div>
+        </div>
+
+    </div>
+    </body>
+</html>
